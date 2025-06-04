a26850706b1014c3b072cace9b986bcd24affd7a
Victoria Lloyd <QAInstructorSky@gmail.com>
2025-06-04 16:33:18 +0100

    EDIT: 3rd test

--- test_bonjour.py
+++ test_bonjour.py
@@ -16,3 +16,12 @@ def test_bonjour_page2():
         assert response.status_code == 200
         assert b"from the" in response.data
         assert b"venue" in response.data
+
+
+
+def test_bonjour_page3():
+    with app.test_client() as test_client:
+        response = test_client.get('/bonjour')
+        assert response.status_code == 200
+        assert b"page" in response.data
+        assert b"para" in response.data
