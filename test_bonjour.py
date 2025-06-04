6fd53d639333e8e3c42c2aa21579bd39887bc8a4
Victoria Lloyd <QAInstructorSky@gmail.com>
2025-06-04 16:26:03 +0100

    EDIT: extra bonjour test

--- test_bonjour.py
+++ test_bonjour.py
@@ -8,3 +8,11 @@ def test_bonjour_page():
         assert b"Bonjour le monde" in response.data
         assert b"Bienvenue" in response.data
         assert b"Version" in response.data
+
+
+def test_bonjour_page2():
+    with app.test_client() as test_client:
+        response = test_client.get('/bonjour')
+        assert response.status_code == 200
+        assert b"from the" in response.data
+        assert b"venue" in response.data
