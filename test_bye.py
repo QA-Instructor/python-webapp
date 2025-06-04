5991f72bb042f3b26e98a344b59fec6c59d719c8
Victoria Lloyd <QAInstructorSky@gmail.com>
2025-06-04 16:07:47 +0100

    EDIT: test_bye.py dummy test

--- test_bye.py
+++ test_bye.py
@@ -8,3 +8,12 @@ def test_bye_page():
         assert b"Goodbye World" in response.data
         assert b"YOUR NAME" in response.data
         assert b"Version" in response.data
+
+
+def test_bye_page2():
+    with app.test_client() as test_client:
+        response = test_client.get('/bye')
+        assert response.status_code == 200
+        assert b"Goodbye" in response.data
+        assert b"YOUR NAME" in response.data
+        assert b"Version" in response.data
